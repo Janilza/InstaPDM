19f314c6c5434c3ababcd5ada9aba89fdabe1643
Janilza <jasimao@ua.pt>
2019-03-20 11:46:37 +0000

    Get step data

---
+++ app/src/main/java/ipg/estg/mcm/instapdm/GetDataOffline.java
@@ -0,0 +1,94 @@
+package ipg.estg.mcm.instapdm;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessActivities;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResult;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GetDataOffline extends AsyncTask<Void, Void, Void> {
+
+    private DataReadRequest readRequest;
+    private String TAG = "GetDataOffline";
+    private GoogleApiClient mClient = null;
+    private DataReadResult dataReadResult;
+    private int total;
+    private float expendedCalories;
+
+
+    GetDataOffline(DataReadRequest dataReadRequest_, GoogleApiClient googleApiClient) {
+        this.readRequest = dataReadRequest_;
+        this.mClient = googleApiClient;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        dataReadResult = Fitness.HistoryApi.readData(mClient, readRequest).await(1, TimeUnit.MINUTES);
+
+        for (Bucket bucket : dataReadResult.getBuckets()) {
+
+            List<DataSet> dataSetx =  bucket.getDataSets();
+
+
+            for (DataSet dataSet : dataSetx) {
+
+                if(dataSet.getDataType().getName().equals("com.google.step_count.delta"))
+                {
+                    if(dataSet.getDataPoints().size() > 0)
+                    {
+                        // total steps
+                        total = total +  dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                    }
+                }
+            }
+            String bucketActivity = bucket.getActivity();
+            if (bucketActivity.contains(FitnessActivities.WALKING) || bucketActivity.contains(FitnessActivities.RUNNING)) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+
+                    if(dataSet.getDataType().getName().equals("com.google.calories.expended")) {
+
+                        for (DataPoint dp : dataSet.getDataPoints()) {
+
+                            if (dp.getEndTime(TimeUnit.MILLISECONDS) > dp.getStartTime(TimeUnit.MILLISECONDS)) {
+                                for (Field field : dp.getDataType().getFields()) {
+                                    // total calories burned
+                                    expendedCalories = expendedCalories + dp.getValue(field).asFloat();
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+
+
+        }
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(Void dataReadResult) {
+        super.onPostExecute(dataReadResult);
+
+        Log.e("GoogleFit", "Steps total is " + total);
+
+        Log.e("GoogleFit", "Total cal is " + expendedCalories);
+
+    }
+}
--- app/src/main/java/ipg/estg/mcm/instapdm/MainActivity.java
+++ app/src/main/java/ipg/estg/mcm/instapdm/MainActivity.java
@@ -2,21 +2,43 @@ package ipg.estg.mcm.instapdm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.firebase.auth.FirebaseAuth;
-import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks{
     private FirebaseAuth mAuth;
     private Toolbar mainToolbar;
+    private GoogleApiClient mClient;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH);
+    private String selectedDate;
 
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView((int) R.layout.activity_main);
+        createFitnessClient();
+        //todo: alterar ppara obetr de um calendario, o user seleciona  data que pretende
+        selectedDate = Calendar.getInstance().getTime().toString(); //o dia de hoje??
     }
 
     public void onStart() {
@@ -29,6 +51,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, LoginActivity.class));
             finish();
         }
+    }
+
+    private void createFitnessClient() {
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addConnectionCallbacks(this)
+                .useDefaultAccount().build();
+        mClient.connect();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,5 +84,79 @@ public class MainActivity extends AppCompatActivity {
     private void sendToLogin() {
         startActivity(new Intent(this, LoginActivity.class));
         finish();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getData(selectedDate);
+    }
+    public void getData(String date) {
+        if (mClient != null && mClient.isConnected() && mClient.hasConnectedApi(Fitness.HISTORY_API)) {
+
+
+            Date d1 = null;
+            try {
+                d1 = dateFormat.parse(date);
+            } catch (Exception ignored) {
+
+            }
+            Calendar calendar = Calendar.getInstance();
+
+            try {
+                calendar.setTime(d1);
+            } catch (Exception e) {
+                calendar.setTime(new Date());
+            }
+
+            DataReadRequest readRequest = getDataByDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+            new GetDataOffline(readRequest, mClient).execute();
+
+            Log.d("HistoryAPI", "Connected");
+
+        }else{
+
+            Log.d("HistoryAPI", "Not connected");
+
+        }
+    }
+
+    private DataReadRequest getDataByDate(int year, int month, int day_of_Month) {
+
+        Calendar startCalendar = Calendar.getInstance(Locale.getDefault());
+
+        startCalendar.set(Calendar.YEAR, year);
+        startCalendar.set(Calendar.MONTH, month);
+        startCalendar.set(Calendar.DAY_OF_MONTH, day_of_Month);
+
+        startCalendar.set(Calendar.HOUR_OF_DAY, 23);
+        startCalendar.set(Calendar.MINUTE, 59);
+        startCalendar.set(Calendar.SECOND, 59);
+        startCalendar.set(Calendar.MILLISECOND, 999);
+        long endTime = startCalendar.getTimeInMillis();
+
+        startCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        startCalendar.set(Calendar.MINUTE, 0);
+        startCalendar.set(Calendar.SECOND, 0);
+        startCalendar.set(Calendar.MILLISECOND, 0);
+        long startTime = startCalendar.getTimeInMillis();
+
+        DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setType(DataSource.TYPE_DERIVED)
+                .setStreamName("estimated_steps")
+                .setAppPackageName("com.google.android.gms")
+                .build();
+
+        return new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
+                .aggregate(ESTIMATED_STEP_DELTAS, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByActivitySegment(1, TimeUnit.MILLISECONDS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
